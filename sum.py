de59b3c8a5d86eaccf9f4267206442468869d9bc
daya-07 <dayaedp07@gmail.com>
2026-04-07 11:52:57 +0530

    Edited sum.py

--- sum.py
+++ sum.py
@@ -2,4 +2,4 @@ n1= int(input("Enter a number: "))
 n2= int(input("Enter a number: "))
 
 sum = n1 + n2
-print("The sum of", n1, "and", n2, "is: ", sum)
+print("The sum of", n1, "and", n2, "is: \n", sum)
